18c8d9d006708ef0bba8da5cb540c54a9d0b0141
Joni Tiainen <jonitia91@gmail.com>
2022-05-07 17:37:13 +0300

    Added jQuery cdn link

--- includes/head.php
+++ includes/head.php
@@ -11,6 +11,8 @@
   
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- jQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <!-- OWN CSS -->
   <link href="css/style.css" rel="stylesheet">
   <title>Datadrivers</title>
