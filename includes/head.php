612aeec71b9b09fb7c34f78ec2a1a92d1f645db9
Joni Tiainen <jonitia91@gmail.com>
2022-05-07 17:41:10 +0300

    jQuery ui cdn link added

--- includes/head.php
+++ includes/head.php
@@ -12,7 +12,9 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- jQuery -->
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <!-- OWN CSS -->
   <link href="css/style.css" rel="stylesheet">
   <title>Datadrivers</title>
