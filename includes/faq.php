ed0935be171c76d9c32c98de70c1873f2745a3f1
veli uurtamo <veli.uurtamo@gmail.com>
2022-05-11 01:15:51 +0300

    updated FAQ page

--- includes/faq.php
+++ includes/faq.php
@@ -1,20 +1,45 @@
 <?php 
   /*
     FAQ page
+    liukutoiminto toimii Firefoxilla ja Edgellä, mutta ei oman koneen Chromella
   */
 ?>
 
 <div class="container-fluid">
-  <h3>kysymys1</h3>
-  <p>Vastaus</p> 
-  <h3>kysymys1</h3>
-  <p>Vastaus</p> 
-  <h3>kysymys1</h3>
-  <p>Vastaus</p> 
-  <h3>kysymys1</h3>
-  <p>Vastaus</p> 
-  <h3>kysymys1</h3>
-  <p>Vastaus</p> 
-  <h3>kysymys1</h3>
-  <p>Vastaus</p> 
+  <div class='faq-container'>
+    <h1>FAQ - Usein kysytyt kysymykset</h1>
+  <div class='otsikko'>
+    <p>Klikkaa kysymystä nähdäksesi vastauksen</p>
+  </div>
+</div>
+  <div id=question1 class='question-area' >
+    <h3>kysymys1</h3>
+  </div>
+  <div id=answer1 class='answer-area'>
+    <p>Vastaus1</p> 
+  </div>
+  <div id=question2 class='question-area' >
+    <h3>kysymys2</h3>
+  </div>
+  <div id=answer2 class='answer-area'>
+    <p>Vastaus2</p> 
+  </div>
+  <div id=question3 class='question-area' >
+    <h3>kysymys3</h3>
+  </div>
+  <div id=answer3 class='answer-area'>
+    <p>Vastaus3</p> 
+  </div>
+  <div id=question4 class='question-area' >
+    <h3>kysymys4</h3>
+  </div>
+  <div id=answer4 class='answer-area'>
+    <p>Vastaus4</p> 
+  </div>
+  <div id=question5 class='question-area' >
+    <h3>kysymys5</h3>
+  </div>
+  <div id=answer5 class='answer-area'>
+    <p>Vastaus5</p> 
+  </div>
 </div>
