732b48d65c6a3b70924e60d47077f20944c6adf1
Joni Tiainen <jonitia91@gmail.com>
2022-05-11 01:36:39 +0300

    Refactored and moved faq function inside to document.ready

--- js/script.js
+++ js/script.js
@@ -3,14 +3,25 @@
 // Establshing no conflict just in case. $ for jQuery works only inside ready function
 $.noConflict();
 jQuery(document).ready(function ($) {
+  /* test.php */
   // hide test form
   $("#test-container").hide();
 
   // hide submit test btn container
   $("#submit-btn-container").hide();
 
-  openModal();
+  /* test.php slides */
+  $("#start-test-btn").click(() => {
+    $("#start-btn-container").hide("slow", "linear", () => {
+      $("#test-container").slideToggle("slow", "linear", () => {
+        $("#submit-btn-container").show("slow");
+      });
+    });
+  });
+  /****END test.php*****/
 
+  /* index.php */
+  openModal();
   function openModal() {
     $("#myModal").addClass("d-flex");
 
@@ -39,51 +50,41 @@ jQuery(document).ready(function ($) {
       $("#myModal").addClass("d-none");
     });
   }
+  /****END index.php*****/
 
-  /* TEST.php */
-  $("#start-test-btn").click(() => {
-    $("#start-btn-container").hide("slow", "linear", () => {
-      $("#test-container").slideToggle("slow", "linear", () => {
-        $("#submit-btn-container").show("slow");
-      });
-    });
-  });
-});
-
-
-// FAQ starts
-
-$(document).ready(function() {
-  $("#question1").click(function() {
+  /* faq.php */
+  faqSlides();
+  function faqSlides() {
+    $("#question1").click(function () {
       $("#answer1").slideToggle("slow");
-  });
-  $("#answer1").click(function() {
+    });
+    $("#answer1").click(function () {
       $("#answer1").slideUp("slow");
-  });
-  $("#question2").click(function() {
-    $("#answer2").slideToggle("slow");
+    });
+    $("#question2").click(function () {
+      $("#answer2").slideToggle("slow");
+    });
+    $("#answer2").click(function () {
+      $("#answer2").slideUp("slow");
+    });
+    $("#question3").click(function () {
+      $("#answer3").slideToggle("slow");
+    });
+    $("#answer3").click(function () {
+      $("#answer3").slideUp("slow");
+    });
+    $("#question4").click(function () {
+      $("#answer4").slideToggle("slow");
+    });
+    $("#answer4").click(function () {
+      $("#answer4").slideUp("slow");
+    });
+    $("#question5").click(function () {
+      $("#answer5").slideToggle("slow");
+    });
+    $("#answer5").click(function () {
+      $("#answer5").slideUp("slow");
+    });
+  }
+  /****END faq.php*****/
 });
-$("#answer2").click(function() {
-    $("#answer2").slideUp("slow");
-});
-$("#question3").click(function() {
-  $("#answer3").slideToggle("slow");
-});
-$("#answer3").click(function() {
-  $("#answer3").slideUp("slow");
-});
-$("#question4").click(function() {
-  $("#answer4").slideToggle("slow");
-});
-$("#answer4").click(function() {
-  $("#answer4").slideUp("slow");
-});
-$("#question5").click(function() {
-  $("#answer5").slideToggle("slow");
-});
-$("#answer5").click(function() {
-  $("#answer5").slideUp("slow");
-});
-});
-
-// FAQ ends
