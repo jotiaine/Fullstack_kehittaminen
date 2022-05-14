9b3c4c423b077545a877b876557460ec804c9df8
Joni Tiainen <jonitia91@gmail.com>
2022-05-15 02:56:40 +0300

    Deleting unnecessary querys, no answer table for example anymore

--- includes/about.php
+++ includes/about.php
@@ -2,8 +2,6 @@
   /*
     ABOUT page
 
-    Alla oleva koodi lisätty helpottamaan yhteyksien testaamista tietokantaan. Näkee
-    nopeammin onko tietokantaan tullut muutoksia sisältöön kuten oli tarkoitus.
   */
 ?>
 
@@ -11,32 +9,28 @@
 <?php
 include('dbConnect.php'); 
 $sql='SELECT * FROM student ORDER by last_name, first_name';  
-$tulos=$conn->query($sql);
-$montako=mysqli_num_rows($tulos);
+$result=$conn->query($sql);
+$rows=mysqli_num_rows($result);
 
 ?>
-<h3>Kaikki opiskelijat  (<?php echo $montako ?>kpl)</h3>
+<h3>All Students  (<?php echo $rows ?>kpl)</h3>
 <table class="table table-striped">
     <thead>
       <tr>
         <th>StudentID</th>
-        <th>CertificateID</th>
-        <th>Etunimi</th>
+        <th>First Name</th>
         <th>Sukunimi</th>
         <th>Email</th>
-        <th>Total_result</th>
       </tr>
     </thead>
     <tbody>
       <?php
-      while($rivi=$tulos->fetch_assoc()){
+      while($row=$result->fetch_assoc()){
           echo '<tr>';
-          echo '<td>'.$rivi['studentID'].'</td>';
-          echo '<td>'.$rivi['certificateID'].'</td>';
-          echo '<td>'.$rivi['first_name'].'</td>';
-          echo '<td>'.$rivi['last_name'].'</td>';
-          echo '<td>'.$rivi['email'].'</td>';
-          echo '<td>'.$rivi['total_result'].'</td>';
+          echo '<td>'.$row['studentID'].'</td>';
+          echo '<td>'.$row['first_name'].'</td>';
+          echo '<td>'.$row['last_name'].'</td>';
+          echo '<td>'.$row['email'].'</td>';
           echo '</tr>';
       }
       //$conn->close(); 
@@ -47,43 +41,41 @@ $montako=mysqli_num_rows($tulos);
 <?php
 //include('dbConnect.php');
 $sql='SELECT * FROM test ORDER by testID';  
-$tulos=$conn->query($sql);
-$montako=mysqli_num_rows($tulos);
+$result=$conn->query($sql);
+$rows=mysqli_num_rows($result);
 ?>
-<br><h3>Kaikki tentit  (<?php echo $montako ?>kpl)</h3>
+<br><h3>All Tests (<?php echo $rows ?>kpl)</h3>
 <table class="table table-striped">
     <thead>
       <tr>
         <th>TestID</th>
         <th>StudentID</th>
-        <th>AnswerID</th>
-        <th>QuestionID</th>
-        <th>Result</th>
+        <th>Question 1</th>
+        <th>Question 2</th>
+        <th>Question 3</th>
       </tr>
     </thead>
     <tbody>
       <?php
-      while($rivi=$tulos->fetch_assoc()){
+      while($row=$result->fetch_assoc()){
           echo '<tr>';
-          echo '<td>'.$rivi['testID'].'</td>';
-          echo '<td>'.$rivi['studentID'].'</td>';
-          echo '<td>'.$rivi['answerID'].'</td>';
-          echo '<td>'.$rivi['questionID'].'</td>';
-          echo '<td>'.$rivi['result'].'</td>';
+          echo '<td>'.$row['testID'].'</td>';
+          echo '<td>'.$row['studentID'].'</td>';
+          echo '<td>'.$row['question_1'].'</td>';
+          echo '<td>'.$row['question_2'].'</td>';
+          echo '<td>'.$row['question_3'].'</td>';
           echo '</tr>';
       }
-      //$conn->close(); 
       ?>
     </tbody>
   </table>
 
-  <?php
-//include('dbConnect.php');
+<?php
 $sql='SELECT * FROM question ORDER by questionID';
-$tulos=$conn->query($sql);
-$montako=mysqli_num_rows($tulos);
+$result=$conn->query($sql);
+$rows=mysqli_num_rows($result);
 ?>
-<br><h3>Kaikki kysymykset  (<?php echo $montako ?>kpl)</h3>
+<br><h3>All Questions (<?php echo $rows ?>kpl)</h3>
 <table class="table table-striped">
     <thead>
       <tr>
@@ -94,88 +86,55 @@ $montako=mysqli_num_rows($tulos);
         <th>correct_answer_1</th>
         <th>correct_answer_2</th>
         <th>correct_answer_3</th>
-
       </tr>
     </thead>
     <tbody>
       <?php
-      while($rivi=$tulos->fetch_assoc()){
+      while($row=$result->fetch_assoc()){
           echo '<tr>';
-          echo '<td>'.$rivi['questionID'].'</td>';
-          echo '<td>'.$rivi['question_1'].'</td>';
-          echo '<td>'.$rivi['question_2'].'</td>';
-          echo '<td>'.$rivi['question_3'].'</td>';
-          echo '<td>'.$rivi['correct_answer_1'].'</td>';
-          echo '<td>'.$rivi['correct_answer_2'].'</td>';
-          echo '<td>'.$rivi['correct_answer_3'].'</td>';
+          echo '<td>'.$row['questionID'].'</td>';
+          echo '<td>'.$row['question_1'].'</td>';
+          echo '<td>'.$row['question_2'].'</td>';
+          echo '<td>'.$row['question_3'].'</td>';
+          echo '<td>'.$row['correct_answer_1'].'</td>';
+          echo '<td>'.$row['correct_answer_2'].'</td>';
+          echo '<td>'.$row['correct_answer_3'].'</td>';
           echo '</tr>';
       }
-      //$conn->close(); 
       ?>
     </tbody>
   </table>
 
-  <?php
-//include('dbConnect.php');
-$sql='SELECT * FROM answer ORDER by answerID';
-$tulos=$conn->query($sql);
-$montako=mysqli_num_rows($tulos);
-?>
-<br><h3>Kaikki vastaukset  (<?php echo $montako ?>kpl)</h3>
-<table class="table table-striped">
-    <thead>
-      <tr>
-        <th>answerID</th>
-        <th>answer_1</th>
-        <th>answer_2</th>
-        <th>answer_3</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      while($rivi=$tulos->fetch_assoc()){
-          echo '<tr>';
-          echo '<td>'.$rivi['answerID'].'</td>';
-          echo '<td>'.$rivi['answer_1'].'</td>';
-          echo '<td>'.$rivi['answer_2'].'</td>';
-          echo '<td>'.$rivi['answer_3'].'</td>';
-          echo '</tr>';
-      }
-      //$conn->close(); 
-      ?>
-    </tbody>
-  </table>
+ 
 
-  <?php
-//include('dbConnect.php');
-$sql='SELECT * FROM message ORDER by messageID';
-$tulos=$conn->query($sql);
-$montako=mysqli_num_rows($tulos);
+<?php
+$sql='SELECT * FROM reward ORDER by studentID';
+$result=$conn->query($sql);
+$rows=mysqli_num_rows($result);
 ?>
-<br><h3>Kaikki viestit  (<?php echo $montako ?>kpl)</h3>
+<br><h3>All Rewards (<?php echo $rows ?>kpl)</h3>
 <table class="table table-striped">
     <thead>
       <tr>
-        <th>messageID</th>
+        <th>rewardID</th>
         <th>studentID</th>
-        <th>message</th>
-        <th>user_type</th>
-        <th>date</th>
+        <th>level</th>
+        <th>certificate</th>
       </tr>
     </thead>
     <tbody>
       <?php
-      while($rivi=$tulos->fetch_assoc()){
+      while($row=$result->fetch_assoc()){
           echo '<tr>';
-          echo '<td>'.$rivi['messageID'].'</td>';
-          echo '<td>'.$rivi['studentID'].'</td>';
-          echo '<td>'.$rivi['message'].'</td>';
-          echo '<td>'.$rivi['user_type'].'</td>';
-          echo '<td>'.$rivi['date'].'</td>';
-          echo '</tr>';
+          echo '<td>'.$row['rewardID'].'</td>';
+          echo '<td>'.$row['studentID'].'</td>';
+          echo '<td>'.$row['level'].'</td>';
+          echo '<td>'.$row['certificate'].'</td>';
+
       }
-      $conn->close(); 
       ?>
     </tbody>
   </table>
+
+ 
 
