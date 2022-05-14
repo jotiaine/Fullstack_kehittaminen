500a07e1656ed92a264a5fe6bb78191c4f55e298
Joni Tiainen <jonitia91@gmail.com>
2022-05-15 01:53:55 +0300

    Feedback update to db is working now, AJAX maybe later

--- includes/feedback.php
+++ includes/feedback.php
@@ -5,6 +5,8 @@
     ********************************
     ** Get students and their tests from db **
     ** and use AJAX to update the part of the site to get more tests & students? **
+    ** Udate Teacher feedback to db****
+    ** First done without AJAX **
     ** JSON prolly not needed here **
     ********************************
     */
@@ -12,10 +14,23 @@
   $sql = "SELECT * FROM test INNER JOIN student ON test.studentID = student.studentID ORDER BY student.last_name, student.first_name, student.studentID";
   $result = $conn -> query($sql);
   $rows = mysqli_num_rows($result);
-  // echo "pre";
-  // echo print_r($rows);
-  // echo "</pre>";
 
+  // Updating feedback to DB using AJAX
+
+
+  if(isset($_POST['submit-feedback'])) {
+    
+  $hiddenStudentID = $conn->real_escape_string($_REQUEST['hiddenStudentID']);
+  $teacher_feedback = $conn->real_escape_string($_POST['teacher_feedback']);
+  echo "<p class='alert alert-success'>". $hiddenStudentID . "</p>";
+  echo "<p class='alert alert-success'>" . $teacher_feedback . "</p>";
+  $sql = "UPDATE test SET teacher_feedback = '$teacher_feedback' WHERE studentID = '$hiddenStudentID'";
+  $result = $conn -> query($sql);
+
+  if($result) echo "<p class='alert alert-success'>Updating feedback for studentID:" . $hiddenStudentID . " is a success!</p>";
+  else echo "<p class='alert alert-warning'>Updating feedback failed</p>";
+  //$conn -> close();
+}
 
 ?>
 
@@ -38,37 +53,40 @@
               
               
               for($x = 0; $x < 1; $x++) {
+                $studentID = $row['studentID'];
+                $first_name = $row['first_name'];
+                $last_name = $row['last_name'];
                 echo "<thead>";
-                echo "<th colspan='2' class='student-name display-3'>" . $row['first_name'] . " " . $row['last_name'] . "</th>";
+                echo "<th colspan='2' class='student-name display-3'>" . $studentID . " " . $first_name . " " . $last_name . "</th>";
                 echo "</thead>";
                 echo "<tbody class='student-body'>";
-
+                
                 foreach($row as $key => $value) {
                   echo "<tr class='student-row table-dark'>";
-                    echo "<td>". $key . "</td>" ;
-                    echo "<td>" . $value . "</td>" ;
+                  echo "<td>". $key . "</td>" ;
+                  echo "<td>" . $value . "</td>" ;
                   echo "</tr>";
                 }
-                echo "<td colspan='2' class='px-4 py-3'>";
                 /* FORM */
-                echo "<form method=''>";
+                echo "<form action='index.php?page=feedback&user=teacher' method='post'>";
+                echo "<td colspan='2' class='px-4 py-3'>";
                 echo "<label for='feedback' class='form-label text-danger'>Feedback</label>";
-                echo "<textarea id='feedback' class='form-control bg-dark text-white border border-danger'>";
+                echo "<textarea name='teacher_feedback' id='feedback' class='form-control bg-dark text-white border border-danger' placeholder='The test went well!' required>";
                 echo "</textarea>";
                 echo "<div class='mt-2'>";
-                echo "<button type='stubmit' name='submit-feedback' class='btn btn-dark text-white'>";
-                echo "Send";
-                echo "</button>";
+                /* Sending studentID hidden in form input */
+                echo "<input type='hidden' name='hiddenStudentID' value='$studentID'>";
+                echo "<input type='submit' name='submit-feedback' class='btn btn-dark text-white' value='Send'>";
                 echo "</div>";
+                echo "</td>";
                 echo "</form>";
                 /* /FORM */
-                echo "</td>";
                 echo "</tbody>";
-
+                
               }
             }
             
-
+            
             $i++;
 
           }
