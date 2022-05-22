4d951f4015afdceecc6f127d67a2d7192d492372
Joni Tiainen <jonitia91@gmail.com>
2022-05-22 20:17:34 +0300

    Test page continues with Jani

--- functions/create_student.php
+++ functions/create_student.php
@@ -25,12 +25,13 @@
                       - Onko oppilas olemassa? if-else
                         - ON:
                           -> Ladataan koko sivu kerran(ei AJAX)
-                          -> Testi tehty? if-else
+                          -> Testi tehty? if
                             - ON: 
                               - Hyväksytty || Hylätty
                                 -> Näytetään testi
+                                TAI
                             - EI:
-                              -> Generoidaan uusi testi
+                              -> Näytetään tekemättä jäänyt testi
                         - EI:
                           -> create_student() - if(isset(submit-student))
                             1. Tallennetaan oppilaan tiedot muuttujiin
@@ -60,11 +61,11 @@
                               - test.json
                               - reward.json
                               - question_series.json
-                            8. Funktio loppuu
+                            8. Luodaan ja näytetään random testi
+                            9. Funktio loppuu
                               - test ja reward taulut
                               - jäävät odottamaan päivityksiä
                               - testien teosta ja palautteista(SQL:UPDATE)
-                            
                             
                             
                             
@@ -77,7 +78,8 @@
 
 <?php // Creating a student to the db
 function create_student() {
-              require('includes/dbConnect.php'); 
+  include('includes/dbConnect.php'); 
+
 
               
               /*
@@ -138,10 +140,10 @@ function create_student() {
                 // Test approved || failed || is not done
                 $testApproved = $score == 1;
                 $testFailed = $score == 0 || $score == NULL && $creationDate != NULL;
-                //$testIsNotDone = $score == 0 && $creationDate == NULL; 
+                $testIsNotDone = $score == 0 && $creationDate == NULL; 
 
                   // Test done
-                  if($testApproved || $testFailed) {
+                  if($testApproved || $testFailed || $testIsNotDone) {
 
                     $sql = "SELECT * FROM student INNER JOIN test ON student.studentID = test.studentID INNER JOIN reward ON student.studentID = reward.studentID WHERE student.studentID = '$studentID'";
   
@@ -307,17 +309,13 @@ function create_student() {
                     echo $output;
 
 
-                    //return ;
                     if($result) {
                       echo "<p class='alert alert-success'>Query success</p>";
                     } else {
                       echo "<p class='alert alert-warning'>Query failed</p>";
                     }
-
-                  } else {
-                    // Test not done
-                    // Generate new test
-                    //return ;
+                    
+                    return;
                   }
 
 
@@ -496,17 +494,148 @@ function create_student() {
   ** TEST & REWARD wait for UPDATES **
   ********************************
   */
+
+
   
+          // Read the JSON file, Testing that it works 
+          $student = file_get_contents('file/student.json');
+          $question_series = file_get_contents('file/question_series.json');
+          $test = file_get_contents('file/test.json');
+          $reward = file_get_contents('file/reward.json');
+          
+          // Decode the JSON file
+          $json_data_student = json_decode($student,true);
+          $json_data_question_series = json_decode($question_series,true);
+          $json_data_test = json_decode($test,true);
+          $json_data_reward = json_decode($reward,true);
+          
+          // Display data
+          echo "<pre>";
+          echo print_r($json_data_student);
+          echo "</pre>";
+          echo "<pre>";
+          echo print_r($json_data_question_series);
+          echo "</pre>";
+          echo "<pre>";
+          echo print_r($json_data_test);
+          echo "</pre>";
+          echo "<pre>";
+          echo print_r($json_data_reward);
+          echo "</pre>";
+          
+
+                // Shuffle options
+                $q1_options = array("opt_1_1", "opt_1_2", "opt_1_3");
+                shuffle($q1_options);
+                $q2_options = array("opt_2_1", "opt_2_2", "opt_2_3");
+                shuffle($q2_options);                   
+                $q3_options = array("opt_3_1", "opt_3_2", "opt_3_3");
+                shuffle($q3_options);
+
+
+
+
+
+
+
+
+               echo "<div id='start-btn-container' class='container-fluid pb-5 pt-3 text-center bg-dark text-white'>";
+               echo "<h2 class='var-header mb-4  shadow-lg p-3'>Ready to start?</h2>";
+               echo "<button id='start-test-btn' class='btn btn-dark text-white'> Show nudes! </button>";
+              echo "</div>";
+            
+             echo "<div id='test-container' class='container-fluid p-5 text-center bg-dark text-white shadow'>";
+             echo "<form action='index.php?page=test&user=student' method='post' class='d-flex flex-column align-items-center justify-content-center'>";
+             echo "<h3 class='var-header mb-3  shadow-lg p-3 w-50'> Test of Mensa </h3>";
+            
+                  
+             echo "<table class='table table-dark text-white table-striped table-hover w-75 text-center text-dark table-bordered shadow-lg'>";
+             echo "<thead class='shadow'>";
+
+                        /// QUESTION 1
+
+                        echo "<th colspan='3' class='var-header p-5 fs-4'>";
+                        echo htmlspecialchars($json_data_question_series["question_1"]); 
+                        echo "</th>";
+                      echo "</thead>";
+                      echo "<tbody class='shadow-lg'>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                          echo "<input class='form-check-input' type='radio' name='question_1' id='answerSerie-1.1' value=" . htmlspecialchars($json_data_question_series[$q1_options[2]]) . ">";
+                          echo "<label class='form-check-label' for='answerSerie-1.1'>" .htmlspecialchars($json_data_question_series[$q1_options[2]]) . "</label>";
+                        echo "</td>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                          echo "<input class='form-check-input' type='radio' name='question_1' id='answerSerie-1.2' value=" .htmlspecialchars($json_data_question_series[$q1_options[1]]) . ">";
+                          echo "<label class='form-check-label' for='answerSerie-1.2'>" . htmlspecialchars($json_data_question_series[$q1_options[1]]) . "</label>";
+                        echo "</td>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                          echo "<input class='form-check-input' type='radio' name='question_1' id='answerSerie-1.3' value=" .htmlspecialchars($json_data_question_series[$q1_options[0]]) . ">";
+                          echo "<label class='form-check-label' for='answerSerie-1.3'>" . htmlspecialchars($json_data_question_series[$q1_options[0]]) . "</label>";
+                        echo "</td>";
+                      echo "</tbody>";
+                      echo "<thead>";
+
+                        // QUESTION 2 
+                        
+                        echo "<th colspan='3' class='var-header p-5 fs-4'>";
+                        echo htmlspecialchars($json_data_question_series['question_2']);
+                        echo "</th>";
+                     echo "</thead>";
+                      echo "<tbody>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                        echo "<input class='form-check-input' type='radio' name='question_2' id='answerSerie-2.1' value=" . htmlspecialchars($json_data_question_series[$q2_options[2]]) . "/>";
+                          echo "<label class='form-check-label' for='answerSerie-2.1'>" . htmlspecialchars($json_data_question_series[$q2_options[2]]) . "</label>";
+                       echo "</td>";
+                       echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                          echo "<input class='form-check-input' type='radio' name='question_2' id='answerSerie-2.2' value=" . htmlspecialchars($json_data_question_series[$q2_options[1]]) . "/>";
+                          echo "<label class='form-check-label' for='answerSerie-2.2'>" . htmlspecialchars($json_data_question_series[$q2_options[1]]) . "</label>";
+                        echo "</td>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                         echo "<input class='form-check-input' type='radio' name='question_2' id='answerSerie-2.3' value=" . htmlspecialchars($json_data_question_series[$q2_options[0]]) . "/>";
+                          echo "<label class='form-check-label' for='answerSerie-2.3'>" .htmlspecialchars($json_data_question_series[$q2_options[0]]) . "</label>";
+                       echo "</td>";
+                     echo "</tbody>";
+                      echo "<thead>";
+
+                        // QUESTION 3 
+
+                        echo "<th colspan='3' class='var-header p-5 fs-4'>";
+                        echo htmlspecialchars($json_data_question_series["question_3"]);
+                        echo "</th>";
+                      echo "</thead>";
+                      echo "<tbody>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                          echo "<input class='form-check-input' type='radio' name='question_3' id='answerSerie-3.1'" .htmlspecialchars($json_data_question_series[$q3_options[2]]) ."/>";
+                          echo "<label class='form-check-label' for='answerSerie-3.1'>" . htmlspecialchars($json_data_question_series[$q3_options[2]]) . "</label>";
+                        echo "</td>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                         echo "<input class='form-check-input' type='radio' name='question_3' id='answerSerie-3.2' value=" . htmlspecialchars($json_data_question_series[$q3_options[1]]) . "/>";
+                          echo "<label class='form-check-label' for='answerSerie-3.2'>" .htmlspecialchars($json_data_question_series[$q3_options[1]]) ." </label>";
+                        echo "</td>";
+                        echo "<td class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>";
+                          echo "<input class='form-check-input' type='radio' name='question_3' id='answerSerie-3.3' value=" .htmlspecialchars($json_data_question_series[$q3_options[0]]) . "/>";
+                          echo "<label class='form-check-label' for='answerSerie-3.3'>" . htmlspecialchars($json_data_question_series[$q3_options[0]]) . "</label>";
+                        echo "</td>";
+                      echo "</tbody>";
+                      
+                    echo "</table>";
+                    
+                    echo "<div id='submit-btn-container' class='container-fluid py-5 text-center bg-dark'>";
+                      echo "<button id='submit-test-btn' class='btn fs-1 btn-primary' type='submit' name='submit-test'> Submit </button>";
+                    echo "</div>";
+            
+                     // <!-- Sending studentId and questionId hidden with submit-test -->
+                     // <!-- <input type="hidden" name="questionID" value="' . $questionID . '">
+                     // "<input type='hidden' name='studentID' value=" . $studentID . '"> -->
+                     
+            
+              echo "</form>";         
+              echo "</div>"; 
   
   
   return $student;
               }
                       
-  
               
 }
 
 ?>
-
-
-  
--- includes/test.php
+++ includes/test.php
@@ -4,7 +4,7 @@
     require('classes/test.php');
     require('classes/student.php');
     require('classes/reward.php');
-    require('functions/create_student.php');
+    include('functions/create_student.php');
 ?>
 
 <?php 
@@ -16,42 +16,7 @@
           // creating student obj, json file, insert empty test & reward
           create_student();
 
-          // Read the JSON file, Testing that it works 
-          $student = file_get_contents('file/student.json');
-          $question_series = file_get_contents('file/question_series.json');
-          $test = file_get_contents('file/test.json');
-          $reward = file_get_contents('file/reward.json');
-          
-          // Decode the JSON file
-          $json_data_student = json_decode($student,true);
-          $json_data_question_series = json_decode($question_series,true);
-          $json_data_test = json_decode($test,true);
-          $json_data_reward = json_decode($reward,true);
-          
-          // Display data
-          echo "<pre>";
-          echo print_r($json_data_student);
-          echo "</pre>";
-          echo "<pre>";
-          echo print_r($json_data_question_series);
-          echo "</pre>";
-          echo "<pre>";
-          echo print_r($json_data_test);
-          echo "</pre>";
-          echo "<pre>";
-          echo print_r($json_data_reward);
-          echo "</pre>";
-          
 
-          // Shuffle options
-          $q1_options = array("opt_1_1", "opt_1_2", "opt_1_3");
-          shuffle($q1_options);
-          $q2_options = array("opt_2_1", "opt_2_2", "opt_2_3");
-          shuffle($q2_options);                   
-          $q3_options = array("opt_3_1", "opt_3_2", "opt_3_3");
-          shuffle($q3_options);
-          
-          
           // Test: read file "new_file.json" & "poista.json" for testing
           // $file_pointer = fopen('file/poista.json', 'r');
           // echo fread($file_pointer, filesize('file/poista.json'));
@@ -68,7 +33,9 @@
           // fclose($file_pointer);
           // unlink('file/poista.json'); // Deletes a file, be careful
 
+          // if($testIsNotDone) {
 
+          // }
         } 
   /*****************/
 ?>
@@ -76,7 +43,7 @@
 
 <?php
     // user ja page turhia?(kun student on sisällä ja vaihtaa sivuja) 
-    if($_GET['user'] == 'student' && $_GET['page'] == 'test' || isset($_POST['submit-test'])) {
+    if(isset($_POST['submit-test'])) {
 
       // Using JSONS to find studentID, first_name and last_name and email
       $json_student = file_get_contents('file/student.json');
@@ -143,6 +110,8 @@
                 shuffle($q2_options);                   
                 $q3_options = array("opt_3_1", "opt_3_2", "opt_3_3");
                 shuffle($q3_options);
+
+
   
 
     }
@@ -205,97 +174,9 @@
     
 
 
-    $conn->close();
 
 
 
     
     ?>
-
-    
-  <div id="start-btn-container" class="container-fluid pb-5 pt-3 text-center bg-dark text-white">
-    <h2 class="var-header mb-4  shadow-lg p-3">Ready to start?</h2>
-    <button id="start-test-btn" class="btn btn-dark text-white"> Show nudes! </button>
-  </div>
-
-  <div id="test-container" class="container-fluid p-5 text-center bg-dark text-white shadow">
-    <form action="index.php?page=test&user=student" method="post" class="d-flex flex-column align-items-center justify-content-center">
-      <h3 class="var-header mb-3  shadow-lg p-3 w-50"> Test of Mensa </h3>
-
-      
-      <table class="table table-dark text-white table-striped table-hover w-75 text-center text-dark table-bordered shadow-lg">
-          <thead class="shadow">
-            <!-- QUESTION 1 -->
-            <th colspan='3' class='var-header p-5 fs-4'>
-              <?php echo htmlspecialchars($question_series['question_1']); ?> 
-            </th>
-          </thead>
-          <tbody class='shadow-lg'>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_1" id="answerSerie-1.1" value="<?php echo htmlspecialchars($question_series[$q1_options[2]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-1.1"> <?php echo htmlspecialchars($question_series[$q1_options[2]]); ?> </label>
-            </td>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_1" id="answerSerie-1.2" value="<?php echo htmlspecialchars($question_series[$q1_options[1]]);  ?>"/>
-              <label class="form-check-label" for="answerSerie-1.2"> <?php echo htmlspecialchars($question_series[$q1_options[1]]);  ?> </label>
-            </td>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_1" id="answerSerie-1.3" value="<?php echo htmlspecialchars($question_series[$q1_options[0]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-1.3"> <?php echo htmlspecialchars($question_series[$q1_options[0]]); ?> </label>
-            </td>
-          </tbody>
-          <thead>
-            <!-- QUESTION 2 -->
-            <th colspan='3' class='var-header p-5 fs-4'>
-              <?php echo htmlspecialchars($question_series['question_2']); ?>
-            </th>
-          </thead>
-          <tbody class=''>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_2" id="answerSerie-2.1" value="<?php echo htmlspecialchars($question_series[$q2_options[2]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-2.1"> <?php echo htmlspecialchars($question_series[$q2_options[2]]); ?> </label>
-            </td>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_2" id="answerSerie-2.2" value="<?php echo htmlspecialchars($question_series[$q2_options[1]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-2.2"> <?php echo htmlspecialchars($question_series[$q2_options[1]]);  ?> </label>
-            </td>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_2" id="answerSerie-2.3" value="<?php echo htmlspecialchars($question_series[$q2_options[0]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-2.3"> <?php echo htmlspecialchars($question_series[$q2_options[0]]); ?> </label>
-            </td>
-          </tbody>
-          <thead>
-            <!-- QUESTION 3 -->
-            <th colspan='3' class='var-header p-5 fs-4'>
-              <?php echo htmlspecialchars($question_series['question_3']); ?>
-            </th>
-          </thead>
-          <tbody class=''>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_3" id="answerSerie-3.1" value="<?php echo htmlspecialchars($question_series[$q3_options[2]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-3.1"> <?php echo htmlspecialchars($question_series[$q3_options[2]]); ?> </label>
-            </td>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_3" id="answerSerie-3.2" value="<?php echo htmlspecialchars($question_series[$q3_options[1]]);  ?>"/>
-              <label class="form-check-label" for="answerSerie-3.2"> <?php echo htmlspecialchars($question_series[$q3_options[1]]);  ?> </label>
-            </td>
-            <td colspan='' class='table-dark text-white p-5 d-flex gap-3 justify-content-left align-items-center'>
-              <input class="form-check-input" type="radio" name="question_3" id="answerSerie-3.3" value="<?php echo htmlspecialchars($question_series[$q3_options[0]]); ?>"/>
-              <label class="form-check-label" for="answerSerie-3.3"> <?php echo htmlspecialchars($question_series[$q3_options[0]]); ?> </label>
-            </td>
-          </tbody>
-          
-        </table>
-        
-        <div id="submit-btn-container" class="container-fluid py-5 text-center bg-dark">
-          <button id="submit-test-btn" class="btn fs-1 btn-primary" type="submit" name="submit-test"> Submit </button>
-        </div>
-
-          <!-- Sending studentId and questionId hidden with submit-test -->
-          <!-- <input type="hidden" name="questionID" value='$questionID'>
-          <input type="hidden" name="studentID" value='$studentID'> -->
-         
-
-  </form>         
-  </div> 
 
