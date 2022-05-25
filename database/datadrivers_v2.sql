fbe0e60f55f47e13e930547c1480b4007522d2fd
Joni Tiainen <jonitia91@gmail.com>
2022-05-25 17:31:18 +0300

    DB sql file updated for others to test

--- database/datadrivers_v2.sql
+++ database/datadrivers_v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2022 at 12:52 AM
+-- Generation Time: May 25, 2022 at 04:29 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -84,25 +84,20 @@ CREATE TABLE `reward` (
 --
 
 INSERT INTO `reward` (`rewardID`, `studentID`, `level`, `certificate`) VALUES
-(3, 112, NULL, NULL),
-(4, 113, NULL, NULL),
-(5, 114, NULL, NULL),
-(6, 115, NULL, NULL),
-(7, 116, NULL, NULL),
-(8, 117, NULL, NULL),
-(9, 118, NULL, NULL),
-(10, 119, NULL, NULL),
-(11, 120, NULL, NULL),
-(12, 121, NULL, NULL),
-(13, 122, NULL, NULL),
-(14, 123, NULL, NULL),
-(15, 124, NULL, NULL),
-(16, 125, NULL, NULL),
-(17, 126, NULL, NULL),
-(18, 127, NULL, NULL),
-(19, 128, NULL, NULL),
-(20, 129, NULL, NULL),
-(21, 130, NULL, NULL);
+(442, 555, NULL, NULL),
+(443, 556, NULL, NULL),
+(444, 557, NULL, NULL),
+(445, 558, NULL, NULL),
+(446, 559, NULL, NULL),
+(447, 560, NULL, NULL),
+(448, 561, NULL, NULL),
+(449, 562, NULL, NULL),
+(450, 563, NULL, NULL),
+(451, 564, NULL, NULL),
+(452, 565, NULL, NULL),
+(453, 566, NULL, NULL),
+(454, 567, NULL, NULL),
+(455, 568, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -123,34 +118,20 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`studentID`, `first_name`, `last_name`, `email`) VALUES
-(103, 'joni', 'joppe', 'joni2@gmail.com'),
-(104, 'dfg', 'dfgdfg', 'df@sdfsdf'),
-(105, 'dfg', 'dfg', 'dfg@dfgdfgdf'),
-(106, 'dfg', 'dfg', 'df@sdfdsf'),
-(107, 'gfd', 'dfg', 'fdg@asfdasd'),
-(108, 'dfg', 'sdfgsd', 'dfasdk@asdasd'),
-(109, 'sdf', 'sdfs', 'df@asdasd'),
-(110, 'dfg', 'dfg', 'dfg@asasd'),
-(111, 'dfg', 'dfgd', 'fg@sdfsdf'),
-(112, 'sdf', 'sdfs', 'df2asdasdas@asd'),
-(113, 'sdf', 'sdf', 'sdf@asdasd'),
-(114, 'sdf', 'sdfs', 'df2asdasd@asdas'),
-(115, 'dfg', 'dfgd', 'fg@asdasd'),
-(116, 'fgh', 'fghfg', 'h@sdfds'),
-(117, 'dfg', 'dfg', 'df2asda@asd'),
-(118, 'dfg', 'dfg', 'df2asdasd@asdasd'),
-(119, 'asd', 'asda', 'sd2asdasdasd@asdasd'),
-(120, 'dfg', 'dfg', 'df@asdsd'),
-(121, 'dsf', 'sdf', 'sdf@asdasd'),
-(122, 'sdf', 'sdfs', 'df@dfsdfggfd'),
-(123, 'dfg', 'dfg', 'dfg@sdgfsdf'),
-(124, 'dfgd', 'fgdf', 'g@sdfgsdf'),
-(125, 'dfg', 'dfg', 'dfg2sdfdf@sdfsdf'),
-(126, 'sdf', 'sdf', 'sdf@dfasdasd'),
-(127, 'dfg', 'dfgd', 'fg@asdasdas'),
-(128, 'dfg', 'dfg', 'sdfsfgdf@asdasdasd'),
-(129, 'sdf', 'sdf', 'sdf@asd'),
-(130, 'dfg', 'dfg', 'dfg@asdasdasdasd');
+(555, 'joku', 'oppilas', 'joku@mail.fi'),
+(556, 'andy', 'oppilas', 'andy@mail.fi'),
+(557, 'uusi', 'oppilas', 'uusi@mail.fi'),
+(558, 'uusi2', 'oppilas', 'uusi2@mail.fi'),
+(559, 'uusi3', 'oppilas', 'uusi3@mail.fi'),
+(560, 'uusi4', 'oppilas', 'uusi4@mail.fi'),
+(561, 'uusi4', 'oppilas', 'uusi@mail.fi'),
+(562, 'uusi5', 'oppilas', 'uusi5@mail.fi'),
+(563, 'mikko', 'mallikas', 'mikko@mail.fi'),
+(564, 'mikko', 'mallikas2', 'mikko2@mail.fi'),
+(565, 'random', 'sukunimi', 'random@mail.fi'),
+(566, 'brad', 'smith', 'brad@mail.fi'),
+(567, 'angelina', 'smith', 'angelina@mail.fi'),
+(568, 'gabriel', 'smith', 'gabriel@mail.fi');
 
 -- --------------------------------------------------------
 
@@ -188,34 +169,20 @@ CREATE TABLE `test` (
 --
 
 INSERT INTO `test` (`testID`, `studentID`, `questionID`, `question_1`, `question_2`, `question_3`, `opt_1_1`, `opt_1_2`, `opt_1_3`, `opt_2_1`, `opt_2_2`, `opt_2_3`, `opt_3_1`, `opt_3_2`, `opt_3_3`, `user_answer_1`, `user_answer_2`, `user_answer_3`, `score`, `teacher_feedback`, `creationDate`) VALUES
-(46, 103, 4, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(47, 104, 7, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(48, 105, 2, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(49, 106, 7, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(50, 107, 9, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(51, 108, 8, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(52, 109, 7, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(53, 110, 1, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(54, 111, 2, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(55, 112, 2, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(56, 113, 6, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(57, 114, 10, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(58, 115, 4, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(59, 116, 6, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(60, 117, 3, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(61, 118, 1, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(62, 119, 9, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1, '0', '2022-05-13'),
-(63, 120, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(64, 121, 9, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(65, 122, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(66, 123, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(67, 124, 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(68, 125, 10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(69, 126, 7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(70, 127, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(71, 128, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(72, 129, 5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(73, 130, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(498, 555, 5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(499, 556, 9, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Palautetta', NULL),
+(500, 557, 9, 'Millainen on sosiaalinen kuljettaja?', 'Olet liittymässä väistämismerkin takaa valtatien liikenteeseen. Vasemmalta lähestyy auto, joka on aikeissa kääntyä tulosuunnastaan katsottuna oikealle. Muita ajoneuvoja ei ole näkyvissä. Miten toimit?', 'Aiot kääntyä risteyksestä. Mikä seuraavista on oikea tapa kertoa aikeesta muille?', 'Sosiaalinen kuljettaja kykenee puhumaan puhelimeen ajaessaan keskittymisen herpaantumatta.', 'Sosiaalinen kuljettaja asettaa muiden tienkäyttäjien tarpeet omiensa edelle.', 'Sosiaalinen kuljettaja ei uskalla ajaa yksin.', 'Lähden poikkeuksellisen ripeästi liikkeelle ennen kuin muita ehtii ilmestyä näköpiiriin.', 'Tilanne on täysin riskitön, joten voin lähteä liikkeelle normaalisti.', 'Annan vasemmalta lähestyvän auton kääntyä ensin, jotta voin varmistaa, ettei sen takana, katveessa, ole ketään.', 'Ensin syttyy suuntamerkki ja vasta sitten jarruvalo.', 'Ensin syttyy jarruvalo ja vasta sen jälkeen suuntamerkki.', 'Jarruvalojen ja suuntamerkin on syytä syttyä yhtäaikaa.', 'Sosiaalinen kuljettaja ei uskalla ajaa yksin.', 'Lähden poikkeuksellisen ripeästi liikkeelle ennen kuin muita ehtii ilmestyä näköpiiriin.', 'Jarruvalojen ja suuntamerkin on syytä syttyä yhtäaikaa.', 0, 'joku kommentti', '2022-05-25'),
+(501, 558, 2, 'Olet kääntymässä liikennevaloista vasemmalle vihreän valon palaessa. Kääntyjille (sinulle) on nuolen muotoinen liikennevalo. Ketä saatat joutua väistämään?', 'Mikä nopeusrajoitus sisältyy pyöräkatu -liikennemerkkiin?', 'Olet kääntymässä liikennevaloista vasemmalle vihreän valon palaessa. Kääntyjille (sinulle) on ympyrän muotoinen liikennevalo. Ketä saatat joutua väistämään?', 'Edestä lähestyviä ajoneuvoja.', 'Vasemmalta lähestyvää kevyttä liikennettä.', 'Väistettäviä ei pitäisi olla.', '20 km/h', '30 km/h', 'Pyöräkatu -liikennemerkki ei sisällä nopeusrajoitusta. Pyöräkadulla nopeus on sovitettava pyöräilijöiden mukaiseksi, tie- tai aluekohtaisen nopeusrajoituksen puitteissa.', 'Edestä lähestyviä ajoneuvoja.', 'Oikealta lähestyvää kevyttä liikennettä.', 'Väistettäviä ei pitäisi olla.', 'Väistettäviä ei pitäisi olla.', 'Pyöräkatu -liikennemerkki ei sisällä nopeusrajoitusta. Pyöräkadulla nopeus on sovitettava pyöräilijöiden mukaiseksi, tie- tai aluekohtaisen nopeusrajoituksen puitteissa.', 'Edestä lähestyviä ajoneuvoja.', 0, NULL, '2022-05-25'),
+(502, 559, 8, 'Miten helpotat omia mahdollisuuksiasi ennakoida?', 'Mikä on törkeän rattijuopumuksen promilleraja?', 'Mistä tiedät, että käyttämäsi lääke saattaa heikentää suorituskykyäsi liikenteessä?', 'Ajamalla aina suurinta sallittua nopeutta.', 'Havainnoimalla mahdollisimman lähelle.', 'Pitämällä riittävästi turvaväliä edellä ajavaan.', '0,2', '0,5', '1,2', 'Tablettien punaisesta väristä.', 'Lääkepaketin kyljessä olevasta punaisesta kolmiosta.', 'Lääkepaketin viivakoodin viimeinen numero on parillinen.', 'Pitämällä riittävästi turvaväliä edellä ajavaan.', '1,2', 'Lääkepaketin kyljessä olevasta punaisesta kolmiosta.', 1, 'NULL', '2022-05-25'),
+(503, 560, 8, 'Miten helpotat omia mahdollisuuksiasi ennakoida?', 'Mikä on törkeän rattijuopumuksen promilleraja?', 'Mistä tiedät, että käyttämäsi lääke saattaa heikentää suorituskykyäsi liikenteessä?', 'Ajamalla aina suurinta sallittua nopeutta.', 'Havainnoimalla mahdollisimman lähelle.', 'Pitämällä riittävästi turvaväliä edellä ajavaan.', '0,2', '0,5', '1,2', 'Tablettien punaisesta väristä.', 'Lääkepaketin kyljessä olevasta punaisesta kolmiosta.', 'Lääkepaketin viivakoodin viimeinen numero on parillinen.', 'Pitämällä riittävästi turvaväliä edellä ajavaan.', '1,2', 'Lääkepaketin kyljessä olevasta punaisesta kolmiosta.', 1, 'NULL', '2022-05-25'),
+(504, 561, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(505, 562, 10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(506, 563, 7, 'Lähestyt liikennevaloja, joissa vilkkuu keltainen liikennevalo. Viereisellä kaistalla on suojatien eteen pysähtynyt auto. Miten toimit?', 'Aiot vaihtaa kaistaa vasemmalle. Mistä peilistä on tässä tilanteessa eniten apua?', 'Miten alkoholi vaikuttaa kuljettajan rutiineihin?', 'Minun on pysähdyttävä, koska suojatien eteen pysähtynyttä autoa ei saa ohittaa pysähtymättä.', 'Voin jatkaa ajamista riitävän alhaisella tilannenopeudella, koska risteyksessä on liikennevalot.', 'Minun on pysähdyttävä tilanteessa vain, jos risteyksessä on tulosuunnastani katsottuna STOP-merkki.', 'Vasemmasta sivupeilistä', 'Oikeasta sivupeilistä', 'Taustapeilistä', 'Nautittuaan alkoholia kuljettajan rutiinit unohtuvat herkemmin.', 'Alkoholilla ei ole vaikutusta kokeneen kuljettajan rutiineihin.', 'Alkoholilla ei ole vaikutusta kuljettajalle muodostuneisiin rutiineihin.', 'Minun on pysähdyttävä tilanteessa vain, jos risteyksessä on tulosuunnastani katsottuna STOP-merkki.', 'Taustapeilistä', 'Nautittuaan alkoholia kuljettajan rutiinit unohtuvat herkemmin.', 0, 'NULL', '2022-05-25'),
+(507, 564, 5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(508, 565, 10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(509, 566, 3, 'Takanasi ajaa poliisiauto, jonka maskissa vilkkuu punainen valo. Miten toimit?', 'Lähestyt liikennevaloja. Sinulle palaa keltainen valo. Minkä väriseksi valot ovat vaihtumassa?', 'Saavut risteykseen, jossa sinulla on STOP-merkki. Mihin kohtaan pysähdyt?', 'Kiihdytän vauhtia, jotta en hidasta poliisin pääsyä kohteeseen.', 'Pyrin etsimään mahdollisimman nopeasti turvallisen paikan, johon voin pysähtyä.', 'Annan poliisille tietä kaistaa vaihtamalla.', 'Vihreäksi', 'Punaiseksi', 'Tulevan valon väriä on mahdoton ennustaa.', 'Mikäli pysäytysviivaa ei ole, STOP-merkin kohdalle.', 'Mikäli pysäytysviivaa ei ole, sellaiseen paikkaan ennen risteävää tietä, josta on esteetön näkyvyys molempiin suuntiin.', 'Mikäli pysäytysviivaa ei ole, aina ennen STOP-merkkiä.', 'Pyrin etsimään mahdollisimman nopeasti turvallisen paikan, johon voin pysähtyä.', 'Punaiseksi', 'Mikäli pysäytysviivaa ei ole, STOP-merkin kohdalle.', 0, 'NULL', '2022-05-25'),
+(510, 567, 4, 'Olet kääntymässä liikennevaloista oikealle vihreän valon palaessa. Kääntyjille (sinulle) on nuolen muotoinen liikennevalo. Ketä saatat joutua väistämään?', 'Olet kääntymässä liikennevaloista vasemmalle vihreän valon palaessa. Kääntyjille (sinulle) on nuolen muotoinen liikennevalo. Ketä saatat joutua väistämään?', 'Lähestyt liikennevaloja, joissa sinulla on myös väistämismerkki. Tarkoituksenasi on jatkaa suoraan. Ketä saatat joutua väistämään liikennevalon vaihduttua vihreäksi?', 'Vasemmalta lähestyviä ajoneuvoja.', 'Takaa lähestyvää, risteävää tietä ylittävää kevyttä liikennettä.', 'Väistettäviä ei pitäisi olla.', 'Edestä lähestyviä ajoneuvoja.', 'Vasemmalta lähestyvää kevyttä liikennettä.', 'Väistettäviä ei pitäisi olla.', 'Vasemmalta lähestyvää autoa.', 'Oikealta lähestyvää autoa.', 'Väistettäviä ei pitäisi olla.', 'Väistettäviä ei pitäisi olla.', 'Väistettäviä ei pitäisi olla.', 'Väistettäviä ei pitäisi olla.', 1, 'NULL', '2022-05-25'),
+(511, 568, 8, 'Miten helpotat omia mahdollisuuksiasi ennakoida?', 'Mikä on törkeän rattijuopumuksen promilleraja?', 'Mistä tiedät, että käyttämäsi lääke saattaa heikentää suorituskykyäsi liikenteessä?', 'Ajamalla aina suurinta sallittua nopeutta.', 'Havainnoimalla mahdollisimman lähelle.', 'Pitämällä riittävästi turvaväliä edellä ajavaan.', '0,2', '0,5', '1,2', 'Tablettien punaisesta väristä.', 'Lääkepaketin kyljessä olevasta punaisesta kolmiosta.', 'Lääkepaketin viivakoodin viimeinen numero on parillinen.', 'Pitämällä riittävästi turvaväliä edellä ajavaan.', '0,2', 'Lääkepaketin viivakoodin viimeinen numero on parillinen.', 0, 'NULL', '2022-05-25');
 
 --
 -- Indexes for dumped tables
@@ -262,19 +229,19 @@ ALTER TABLE `question`
 -- AUTO_INCREMENT for table `reward`
 --
 ALTER TABLE `reward`
-  MODIFY `rewardID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `rewardID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=456;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `studentID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=131;
+  MODIFY `studentID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=569;
 
 --
 -- AUTO_INCREMENT for table `test`
 --
 ALTER TABLE `test`
-  MODIFY `testID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `testID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=512;
 
 --
 -- Constraints for dumped tables
